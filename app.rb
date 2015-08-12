e93be27ea1e5d901e6048c0e0fd43f6a71d0366f
Dennis Walters <dwalters@engineyard.com>
2015-08-12 14:50:32 -0400

    made worky like

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'hobbit'
 
 class App < Hobbit::Base
   get '/' do
-    STDOUT.write("This is going to the terminal at #{Time.now.utc}\n")
-    "The time is #{Time.now.utc}"
-    puts "Obligatory timestamp #{Time.now.utc}"
+    now = Time.now.utc
+    puts("Obligatory app timestamp - #{now}")
+    "The time is #{now}"
   end
 end
